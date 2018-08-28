9b556f6953911d2563b431565117755a1aabf397
replicatesyndicate <40871428+replicatesyndicate@users.noreply.github.com>
2018-08-28 14:17:27 +0300

    added final version

--- tictactoe_final.py
+++ tictactoe_final.py
@@ -7,30 +7,27 @@ def tictactoe():
     board_clear = ["_", " ", " ", " ", " ", " ", " ", " ", " ", " "]
     board_example = ["_", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     player = {"X":"","O":""}
-    #valid_move = False
     goal = "play"
     board_index = 0    
-    #################################################################################
+    #############################################################################################################
     
     #FUNCTIONS
     
-    #Turn function: Executes player turns, and applies valid moves.        
+    #Turn function: Executes player turns, and returns valid moves.        
     def user_turn(order):
-        valid = False
-        while valid == False:
-            
-            #Print the player turns
-            board_input = int(input("Your turn, Player {}. Please enter cell number.".format(player[order])))
-                        
-            #check if move is valid, and request another input if invalid move.
-            if board_input in range(1,10):
-                valid = board[board_input] == " "
-                if valid == False: 
-                    print("Invalid move.")
-            else:
+        
+        while True:
+            try:
+                board_input = int(input("Your turn, Player {}. Please enter cell number.".format(player[order])))
+                #check if input is valid, if not raise error and try again.
+                if board_input in range(1,10) and board[board_input] == " ":
+                    break
+                else:
+                    raise BadIndexError()
+                                        
+            except:
                 print("Please enter a number in range 1-9")
                 
-        #if valid move, return move. switch players for next turn.
         return board_input
         
     
@@ -81,7 +78,7 @@ def tictactoe():
             else:
                 return "draw"
 
-   ###############################################################################   
+   ###########################################################################################################   
     
     #executing
     while replay == "Y":
@@ -91,11 +88,20 @@ def tictactoe():
         print("Try lining up your characters horizontally, vertically, or diagonally to win!")
         print("Indicate placement location by using numbers ranging 1 to 9.")
         time.sleep(3)
-        board = board_example
+        board = board_example.copy()
         display_board()
         print("The cell positions corresponding to numbers are shown above. Using a numpad is recommended.")
-        board = board_clear.copy() #takes a copy of board_clear and assigns it to board. use to avoid overwriting board_clear.
-        player_order = input("Player 1, please choose X/O:").upper()
+        board = board_clear.copy()
+        
+        while True: #ask for starting players, if mark isn't given as X/O raise error and try again.
+            try:
+                player_order = input("Player 1, please choose X/O:").upper()
+                if player_order not in ("X","O"):
+                    raise MarkTypeError()
+                break
+            except:
+                print("Please choose X/O as your mark!")
+                
         if player_order == "X":
             print("Player 1 will go first!")
             player["X"] = "1"
@@ -121,8 +127,7 @@ def tictactoe():
             elif player_order == "O":
                 player_order = "X"
             goal = goal_check(game_turns)
-            #print(goal) #debugger
-                   
+                               
         #Game over screen
         display_board()
         if goal == "X" or goal == "O":
@@ -132,11 +137,10 @@ def tictactoe():
         
         #Resetting and replaying
         board = board_clear.copy()
-        
         goal = "play"
-        #display_board() #debugger
         replay = input("Replay? Y/N").upper()
 
         clear_output()  
     return
-    
+#################################################################################################################
+tictactoe()
